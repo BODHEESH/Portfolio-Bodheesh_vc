21b263657eda173605bf66302fa9774975572823
bodheesh <vcbodheesh@gmail.com>
2022-12-19 19:22:26 +0530

    live chat added

--- assests/js/script.js
+++ assests/js/script.js
@@ -81,6 +81,20 @@ function(){
     // })();
 // End of Tawk.to Live Chat
 
+// Start of Tawk.to Script
+
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/63a036dab0d6371309d51bb4/1gkktci81';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+
+// End of Tawk.to Script
+
  // <!-- emailjs to mail contact form data -->
  $("#submit-form").submit(function (event) {
     //event.preventdefault();
